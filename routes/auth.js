be85c3cf015db1a920faa0d161d7e26b05658cf3
Srikanth <63461924+Srikanth2355@users.noreply.github.com>
2023-05-06 21:39:02 +0530

    added validations to signup and login (#5)

--- routes/auth.js
+++ routes/auth.js
@@ -2,15 +2,31 @@ const express = require('express')
 const router = express.Router()
 const authcontroller = require('../controllers/auth')
 
-router.get('/login',authcontroller.getLogin)
+const { check,body } = require('express-validator')
 
-router.post('/login',authcontroller.postLogin)
+
+router.get('/login', authcontroller.getLogin)
+
+router.post('/login',
+    check('email').isEmail().withMessage('Please enter valid Email').normalizeEmail() ,
+    body('password','Password should be minimum 5 characters').isLength({'min':5}).trim(), 
+    authcontroller.postLogin)
 
 router.post('/logout', authcontroller.postLogout);
 
-router.get('/signup', authcontroller.getSignup);
+router.get('/signup',  authcontroller.getSignup);
 
-router.post('/signup', authcontroller.postSignup);
+router.post('/signup', 
+    check('email').isEmail().withMessage('Please enter valid Email').normalizeEmail() ,
+    body('password','Password should be minimum 5 characters').isLength({'min':5}).trim(),
+    body('confirmPassword','Password and confirm password doesnot match').
+    custom((value,{req})=>{
+        if(value !== req.body.password){
+            throw new Error('Passwords do not match')
+        }
+        return true
+    }).trim(),
+    authcontroller.postSignup);
 
 router.get('/reset',authcontroller.getReset);
 
